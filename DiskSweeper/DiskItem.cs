a548e334acc6aebcfc34930e9e2ce72663404d40
Yang Wang <luanshixia@gmail.com>
2018-07-13 16:09:09 -0700

    Add project DiskSweeper

---
+++ DiskSweeper/DiskItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiskSweeper
+{
+    public class DiskItem
+    {
+        public DiskItemType Type { get; set; }
+        public string Name { get; set; }
+        public long Size { get; set; }
+    }
+
+    public enum DiskItemType
+    {
+        Unknown,
+        File,
+        Directory
+    }
+}
